6ac24cd4ba8cf2d323801de51c8556a618db6a53
Szczepan Rędzioch <szczepan@redzioch.com>
2023-04-19 15:25:48 +0200

    Update provider.go

--- provider/provider.go
+++ provider/provider.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/hashicorp/terraform/helper/hashcode"
-	"github.com/tpretz/go-zabbix-api"
+	"github.com/redzioch/go-zabbix-api"
 )
 
 // Provider definition
